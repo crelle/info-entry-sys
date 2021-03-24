bf4525032d6f5cd6271b1b7475263bcca2e70eee
crelle <383362161@qq.com>
2021-03-24 14:26:04 +0800

    1426

--- src/main/java/plant/common/config/SecurityConfig.java
+++ src/main/java/plant/common/config/SecurityConfig.java
@@ -6,14 +6,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import plant.common.ResponseResult;
+import plant.common.util.ResultUtils;
+import plant.model.entity.User;
 import plant.service.impl.UserServiceImpl;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
@@ -27,16 +45,15 @@ import java.io.PrintWriter;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserServiceImpl userService;
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+
+    @Autowired
+    CustomFilterInvocationSecurityMetadataSource customFilterInvocationSecurityMetadataSource;
+    @Autowired
+    CustomUrlDecisionManager customUrlDecisionManager;
 
     @Bean
-    RoleHierarchy roleHierarchy() {
-        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
-        hierarchy.setHierarchy("ROLE_admin > ROLE_user");
-        return hierarchy;
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
@@ -46,54 +63,95 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/js/**", "/css/**", "/images/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/login.html", "/img/**", "/fonts/**", "/favicon.ico", "/verifyCode");
+    }
+
+    @Bean
+    LoginFilter loginFilter() throws Exception {
+        LoginFilter loginFilter = new LoginFilter();
+        loginFilter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandler() {
+            @Override
+            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                response.setContentType("application/json;charset=utf-8");
+                PrintWriter out = response.getWriter();
+                User user = (User) authentication.getPrincipal();
+                user.setPassword(null);
+                ResponseResult responseResult = ResultUtils.success("登录成功!", user);
+                String s = new ObjectMapper().writeValueAsString(responseResult);
+                out.write(s);
+                out.flush();
+                out.close();
+            }
+        });
+        loginFilter.setAuthenticationFailureHandler(new AuthenticationFailureHandler() {
+            @Override
+            public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                response.setContentType("application/json;charset=utf-8");
+                PrintWriter out = response.getWriter();
+                ResponseResult responseResult = ResultUtils.fail(exception.getMessage());
+                if (exception instanceof LockedException) {
+                    responseResult.setMessage("账户被锁定，请联系管理员!");
+                } else if (exception instanceof CredentialsExpiredException) {
+                    responseResult.setMessage("密码过期，请联系管理员!");
+                } else if (exception instanceof AccountExpiredException) {
+                    responseResult.setMessage("账户过期，请联系管理员!");
+                } else if (exception instanceof DisabledException) {
+                    responseResult.setMessage("账户被禁用，请联系管理员!");
+                } else if (exception instanceof BadCredentialsException) {
+                    responseResult.setMessage("用户名或者密码输入错误，请重新输入!");
+                }
+                out.write(new ObjectMapper().writeValueAsString(responseResult));
+                out.flush();
+                out.close();
+            }
+        });
+        loginFilter.setAuthenticationManager(authenticationManagerBean());
+        loginFilter.setFilterProcessesUrl("/doLogin");
+        return loginFilter;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("admin")
-//                .antMatchers("/user/**").hasRole("user")
-//                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginProcessingUrl("/doLogin")
-                .successHandler((req, resp, authentication) -> {
-                    Object principal = authentication.getPrincipal();
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write(new ObjectMapper().writeValueAsString(principal));
-                    out.flush();
-                    out.close();
+                .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
+                    @Override
+                    public <O extends FilterSecurityInterceptor> O postProcess(O object) {
+                        object.setAccessDecisionManager(customUrlDecisionManager);
+                        object.setSecurityMetadataSource(customFilterInvocationSecurityMetadataSource);
+                        return object;
+                    }
                 })
-                .failureHandler((req, resp, e) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write(e.getMessage());
-                    out.flush();
-                    out.close();
-                })
-                .permitAll()
                 .and()
                 .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessHandler((req, resp, authentication) -> {
-                    resp.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = resp.getWriter();
-                    out.write("注销成功");
-                    out.flush();
-                    out.close();
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest req, HttpServletResponse resp, Authentication authentication) throws IOException, ServletException {
+                        resp.setContentType("application/json;charset=utf-8");
+                        PrintWriter out = resp.getWriter();
+                        out.write(new ObjectMapper().writeValueAsString(ResultUtils.success("注销成功!")));
+                        out.flush();
+                        out.close();
+                    }
                 })
                 .permitAll()
                 .and()
                 .csrf().disable().exceptionHandling()
-                .authenticationEntryPoint((req, resp, authException) -> {
-                            resp.setContentType("application/json;charset=utf-8");
-                            PrintWriter out = resp.getWriter();
-                            out.write("尚未登录，请先登录");
-                            out.flush();
-                            out.close();
+                //没有认证时，在这里处理结果，不要重定向
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest req, HttpServletResponse resp, AuthenticationException authException) throws IOException, ServletException {
+                        resp.setContentType("application/json;charset=utf-8");
+                        resp.setStatus(401);
+                        PrintWriter out = resp.getWriter();
+                        ResponseResult responseResult = ResultUtils.fail("访问失败!");
+                        if (authException instanceof InsufficientAuthenticationException) {
+                            responseResult.setMessage("请求失败，请联系管理员!");
                         }
-                );
+                        out.write(new ObjectMapper().writeValueAsString(responseResult));
+                        out.flush();
+                        out.close();
+                    }
+                });
+        http.addFilterAt(loginFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
---
+++ src/main/java/plant/common/util/ResultUtils.java
@@ -0,0 +1,50 @@
+package plant.common.util;
+
+import io.swagger.annotations.ApiModelProperty;
+import plant.common.ResponseResult;
+import plant.common.enumeration.ResponseEnum;
+
+import java.io.Serializable;
+
+/**
+ * @author:crelle
+ * @className:ResultUtils
+ * @version:1.0.0
+ * @date:2021/3/24
+ * @description:XX
+ **/
+public class ResultUtils {
+
+
+    public ResultUtils() {
+    }
+
+    public static <T> ResponseResult<T> build() {
+        return new ResponseResult();
+    }
+
+    public static <T> ResponseResult<T> success() {
+        return new ResponseResult(ResponseEnum.SUCCESS.getCode(), ResponseEnum.SUCCESS.getName(), null);
+    }
+
+    public static <T> ResponseResult<T> success(String message) {
+        return new ResponseResult(ResponseEnum.SUCCESS.getCode(), message, null);
+    }
+
+    public static <T> ResponseResult<T> success(String message, T data) {
+        return new ResponseResult(ResponseEnum.SUCCESS.getCode(), message, data);
+    }
+
+    public static <T> ResponseResult<T> fail() {
+        return new ResponseResult(ResponseEnum.FAIL.getCode(), ResponseEnum.FAIL.getName(), null);
+    }
+
+    public static <T> ResponseResult fail(String message) {
+        return new ResponseResult(ResponseEnum.FAIL.getCode(), message, null);
+    }
+
+    public static <T> ResponseResult fail(String message, Object data) {
+        return new ResponseResult(ResponseEnum.FAIL.getCode(), message, data);
+    }
+
+}
