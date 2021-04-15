1fbbd6cb38238d7b61487c3280d6d7d4bad1faa8
crelle <383362161@qq.com>
2021-04-15 11:26:27 +0800

    解决父子菜单嵌套循环问题

--- src/main/java/crelle/family/model/entity/Menu.java
+++ src/main/java/crelle/family/model/entity/Menu.java
@@ -75,12 +75,14 @@ public class Menu {
 
 
     @ApiModelProperty(value = "子菜单集合", hidden = true)
+    @JsonIgnoreProperties(value = "parentMenu")
     @OneToMany(targetEntity = Menu.class, mappedBy = "parentMenu", fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private Set<Menu> childrenMenus = new HashSet<>();
 
     //解决循环嵌套问题，忽略关联对象任意一方的结果输出
     @ApiModelProperty(value = "父亲菜单", hidden = true)
 //    @JsonIgnore
+    @JsonIgnoreProperties(value = "childrenMenus")
     @ManyToOne(targetEntity = Menu.class, fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     @JoinColumn(name = "parent_id", referencedColumnName = "id")
     private Menu parentMenu;
