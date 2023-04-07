3f1a439292564619684b5013dfdee99b29b98ccc
crelle <383362161@qq.com>
2023-04-07 15:23:23 +0800

    sql脚本升级

--- src/main/resources/sql-upgrade-script/ddl/ddl_230407.sql
+++ src/main/resources/sql-upgrade-script/ddl/ddl_230407.sql
@@ -1,3 +1,3 @@
-ALTER TABLE `archermind-system-db`.`t_menu`
+ALTER TABLE `t_menu`
 CHANGE COLUMN `menu_sort` `menu_sort` VARCHAR(64) NOT NULL COMMENT '菜单排序标识' ,
 ADD UNIQUE INDEX `menu_sort_UNIQUE` (`menu_sort` ASC);
