95bc06795db42fefc34a83e66e441f4f6018866b
crelle <383362161@qq.com>
2023-04-25 11:10:59 +0800

    客户关联查询

--- src/main/java/baseline/app/pojo/vo/CustomerVo.java
+++ src/main/java/baseline/app/pojo/vo/CustomerVo.java
@@ -27,39 +27,33 @@ public class CustomerVo {
     private String id;
 
     @ApiModelProperty("客户名称")
-    @TableField("name")
     private String name;
 
     @ApiModelProperty("地域编号")
     private String regionId;
 
     @ApiModelProperty("地域")
-    @TableField("region_name")
     private String regionName;
 
     @ApiModelProperty("办公地点")
-    @TableField("address")
     private String address;
 
     @ApiModelProperty("负责人ID")
-    @TableField("user_id")
     private String userId;
 
+    @ApiModelProperty("负责人姓名")
+    private String userName;
+
     @ApiModelProperty("联系电话")
-    @TableField("cell_phone")
     private String cellPhone;
 
     @ApiModelProperty("电子邮箱")
-    @TableField("email")
     private String email;
 
     @ApiModelProperty("客户介绍")
-    @TableField("introduce")
     private String introduce;
 
-
     @ApiModelProperty("修改时间")
-    @TableField("update_time")
     private Date updateTime;
 
 
